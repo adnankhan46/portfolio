436f3e24e8512985d17289df0946d286f9a43a41
Adnan <technicaljigsaw.tj@gmail.com>
2026-01-03 16:44:01 +0530

    refactor : experience descriptions

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,17 +1,30 @@
 import { Experience } from "@/lib/types";
 
+const freelanceDesc =`Involved in building Production grade, <strong>type-safe React Native App</strong>.
+- Built entire app using React Native Expo consist of <strong>80+ APIs</strong>
+- Serving to <strong>real</strong> users
+- Implemented Client Side Payment system using <strong>Razorpay</strong>
+- Implemented <strong>Chat Systems</strong> in both app, optimized for scale.
+`
+const internDesc =`Rebuilt entire web stack using <strong>React.js</strong> and <strong>Firebase Cloud</strong>
+- Improved site performance by <strong>70%</strong> by reducing unneccessary loads
+- Optimized Components for improved performance and scalibility
+- Continously contributed to the repo by discussing with founders
+`
+
 export const experience: Experience[] = [
   {
     title: "Freelance Cross-Plateform App Developer",
     company: "Stealth",
     period: "March 2025 - present",
-    description: "Continuously building and shipping production ready apps",
+    description: freelanceDesc,
   },
   {
     title: "Web Dev intern",
     company: "Starlight Foundation",
     period: "Feb 2024 - Apr 2024",
-    description:
-      "Rebuilt entire web stack using React.js and Firebase Cloud \n -Improved site performance by <strong>70%</strong> by reducing unneccessary loads \n -Optimized Components for improved performance and scalibility \n -Continously contributed to the repo by discussing with founders",
+    description: internDesc
   },
 ];
+
+
